57a7401c671833f355a4fe132bcea4d61a2f2ff5
Pavel <podoprigoraps@gmail.com>
2023-11-09 04:14:21 +0400

    task 2 build

--- task-2/src/router/index.js
+++ task-2/src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
+  mode: 'history',
+  base: '/bt-21-tasks/',
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
