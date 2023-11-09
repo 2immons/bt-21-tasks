6b0b5cad3d674367111828c7fd3842b479c72a79
Pavel <podoprigoraps@gmail.com>
2023-11-09 04:26:35 +0400

    Update index.js

--- task-2/src/router/index.js
+++ task-2/src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   mode: 'history',
-  base: 'https://github.com/2immons/bt-21-tasks',
+  base: '/2immons/bt-21-tasks/',
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
